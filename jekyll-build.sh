bd5c190288326e5512fa554320e149847af196bb
xta0 <jayson.xu@foxmail.com>
2018-08-21 17:19:05 -0400

    Update(auto commit)

--- jekyll-build.sh
+++ jekyll-build.sh
@@ -17,7 +17,7 @@ build(){
     echo "Pulling new commits..."
     git pull origin master
     echo "Running jekyll build, this may take a while...."
-    jekyll build || "jekyll build failed!"
+    jekyll build JEKYLL_ENV=production || "jekyll build failed!"
     if [ "$?" -eq "0" ]; then
         echo "Build Succeed"
     else
