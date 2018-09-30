d2c32641fd53ea77a94fe664de4af14ceec9287c
xta0 <jayson.xu@foxmail.com>
2018-09-30 16:41:04 -0400

    Update(auto commit)

--- server.js
+++ server.js
@@ -1,30 +1,31 @@
-const express = require('express')
-const bodyparser = require ('body-parser')
-const indexRouter = require('./route/index')
-const webhookRouter = require('./route/webhook')
-const logger = require('./utils/logger').default_logger
-const config = require('./config/config')
+const express = require('express');
+const bodyparser = require('body-parser');
+const helmet = require('helmet');
+const indexRouter = require('./route/index');
+const webhookRouter = require('./route/webhook');
+const logger = require('./utils/logger').default_logger;
+const config = require('./config/config');
 
-const app = express()
+const app = express();
+
 //middleware
-//body parser
-app.use(bodyparser.json())
-app.use(bodyparser.urlencoded({extended:false}))
+app.use(helmet());
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: false }));
 
 //bunyan log
-app.use(function(req,res,next){
-    logger.info({
-        header: JSON.stringify(req.headers),
-        body: JSON.stringify(req.body)
-    })
-    next()
-})
+app.use(function(req, res, next) {
+  logger.info({
+    header: JSON.stringify(req.headers),
+    body: JSON.stringify(req.body)
+  });
+  next();
+});
 
 //router
-app.use('/api',indexRouter)
-app.use('/api/webhook',webhookRouter)
+app.use('/api', indexRouter);
+app.use('/api/webhook', webhookRouter);
 
-app.listen(config.port,"127.0.0.1", function(){
-    logger.info("Server Started!")
-})
-
+app.listen(config.port, '127.0.0.1', function() {
+  logger.info('Server Started!');
+});
